95171bd3c615b7d1bee7dd87aa033a5e5dabadd3
Casiz <casazzagabriele@gmail.com>
2024-04-30 10:37:18 +0200

    Init

---
+++ main.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+const toDo_num = document.getElementById("num")
+const button = document.getElementById("enter_button")
+let toDo_List = []
+
+button.addEventListener("click", (event) => {
+    toDo_num.innerText = toDo_list.lenght
+});
