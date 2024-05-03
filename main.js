4faa9a00d51d3929bac6a3902460d0b26dd692eb
Casiz <casazzagabriele@gmail.com>
2024-05-03 21:49:03 +0200

    ProjectUp

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 import './style.css'
 
-const toDo_num = document.getElementById("num")
 const button = document.getElementById("enter_button")
-let toDo_List = []
+
 
 button.addEventListener("click", (event) => {
     toDo_num.innerText = toDo_list.lenght
