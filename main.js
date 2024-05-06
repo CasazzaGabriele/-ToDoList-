3e736b79e16b19acdae50b5815bf1f5b519080d0
Casiz <casazzagabriele@gmail.com>
2024-05-06 15:31:02 +0200

    add, and delate

--- main.js
+++ main.js
@@ -3,77 +3,108 @@ import './style.css'
 const button = document.getElementById("button_input_enter")
 const input_field = document.getElementById("input_toDO")
 const toDo_Container = document.getElementById("div_container_toDo_list")
-
 let toDo_list = []
 
-const toDoElement = {
-    text: "",
-    isDone: false,
 
-}
+function Create_html_Todo_Element(todo){  
 
-
-
-function create_ToDo_Elememt_Constructor (todo) {  
-    let div_ToDo_Component = document.createElement("div")
-    div_ToDo_Component.className ="div_toDo_component"
-    toDo_Container.appendChild(div_ToDo_Component)
-
-    let p = document.createElement("p")
-    p.className ="text_toDo"
-    p.innerText = todo.text
-    div_ToDo_Component.appendChild(p)
-
-    let checkButton = document.createElement("button")
-    checkButton.className = "check_toDo"
-    div_ToDo_Component.appendChild(checkButton)
-
-    let icon_check = document.createElement("span")
-    icon_check.innerText = "Check"
-    icon_check.className = "material-symbols-outlined"
-    checkButton.appendChild(icon_check)
-
-    let button_delate_toDo = document.createElement("button")
-    button_delate_toDo.className = "button_delate_toDo"
-    div_ToDo_Component.appendChild(button_delate_toDo)
-    
-    let icon_trash = document.createElement("i")
-    icon_trash.className = "gg-trash"
-    button_delate_toDo.appendChild(icon_trash)
-
-    return {
-        div_container_element: div_ToDo_Component,
-        paragraf: p,
-        button_check: checkButton,
-        button_delate: button_delate_toDo,
-        delate_object: this.button_delate.addEventListener("click",(event)=>{
-            toDo_Container.removeChild(this.div_container_element)
-        })
+    let object_Todo = {
+        div_container_element: {
+            html:     document.createElement("div"),
+            },
+        paragraf:     {
+            html:     document.createElement("p"),
+            text:     todo
+            },
+        button_check: {
+            html:     document.createElement("button"),
+            isDone:    false
+            },
+        icon_check:   {
+            html:     document.createElement("span"),
+            },
+        button_delate: {
+            html:      document.createElement("button"),
+            },
+        icon_trash:{    
+            html:      document.createElement("i"),
+            }
     }
+    
+    // DIV_CONTAINER
+    object_Todo.div_container_element.html.className ="div_toDo_component"
+    toDo_Container.appendChild(object_Todo.div_container_element.html)
+
+    // PARAGRAF
+    object_Todo.paragraf.html.className = "text_toDo"
+    object_Todo.paragraf.html.innerText = object_Todo.paragraf.text
+    object_Todo.div_container_element.html.appendChild(object_Todo.paragraf.html)
+
+    // BUTTON_CHECK
+    object_Todo.button_check.html.className = "check_toDo"
+    object_Todo.div_container_element.html.appendChild(object_Todo.button_check.html)
+
+    // ICON_CHECK
+    object_Todo.icon_check.html.className = "material-symbols-outlined"
+    object_Todo.icon_check.html.innerText = "Check"
+    object_Todo.button_check.html.appendChild(object_Todo.icon_check.html)
+
+    // BUTTON_DELATE
+    object_Todo.button_delate.html.className = "button_delate_toDo"
+    object_Todo.div_container_element.html.appendChild(object_Todo.button_delate.html)   
+
+    // ICON_TRASH
+    object_Todo.icon_trash.html.className = "gg-trash"
+    object_Todo.button_delate.html.appendChild(object_Todo.icon_trash.html)
+
+
+    // EVENT ON CLICK BUTTON CHECK TODOLIST
+    object_Todo.button_check.html.addEventListener("click", (event) =>{
+        object_Todo.button_check.isDone = !object_Todo.button_check.isDone
+        if(object_Todo.button_check.isDone){
+            object_Todo.div_container_element.html.style.backgroundColor ='red';
+            object_Todo.paragraf.html.style.textDecoration ='line-through';
+        }
+        else{
+            object_Todo.div_container_element.html.style.backgroundColor = '#3332325b';
+            object_Todo.paragraf.html.style.textDecoration ='none';
+            }
+    })
+
+    // EVENT ON CLICK BUTTON DELATE TODOLIST
+    object_Todo.button_delate.html.addEventListener("click", (event)=>{
+        toDo_Container.removeChild(object_Todo.div_container_element.html)
+        toDo_list.pop(object_Todo)
+    })
+
+    return object_Todo
 }
 
 button.addEventListener("click",(event)=>{
-    if (toDo_list.length != 0){
-        for (var element of toDo_list){
-            if (element.text == input_field.value){    
-                break
-            }
-            else{
-                let toDo_Object = toDoElement;
-                toDo_Object.text = input_field.value
-                toDo_Object.isDone = false
-                toDo_list(create_ToDo_Elememt_Constructor(toDo_Object))
+    // IS THE INPUT FILLED ?
+    if(input_field.value != "" ){
+        // IS THE LIST FILLED ?
+        if (toDo_list.length != 0){
+            for (var element of toDo_list){
+                if (element.paragraf.text != input_field.value){ 
+                    console.log(input_field.value)
+                    toDo_list.push(Create_html_Todo_Element(input_field.value)) 
+                    console.log(toDo_list) 
+                    input_field.value = ""
+                    return null
+                }
             }
         }
+        else{
+            toDo_list.push(Create_html_Todo_Element(input_field.value))    
+            console.log(toDo_list)  
+            input_field.value = ""
+            return null
+        }
+        
+    }else{
+        input_field.value = ""
+        return null
     }
-    else{
-        console.log("canee")
-        let toDo_Object = toDoElement;
-        toDo_Object.text = input_field.value
-        toDo_Object.isDone = false
-        toDo_list.push(toDo_Object)
-        toDo_list(create_ToDo_Elememt_Constructor(toDo_Object))
-    }
-    input_field.value =""
 });
      
